245df1c2d7545204cdafbafa6e96bbd7eff80f58
Marwan-G <ghabin2004@gmail.com>
2017-07-24 20:33:24 +0200

    creating folder structures and our app components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import {AppRecipes}from '../app/recipes/recipes.component';
+import {AppHeader} from '../app/header/header.component';
+import {AppRecipeDetail} from '../app/recipes/recipe-detail/recipe-detail.component';
+import {AppRecipeItem} from '../app/recipes/recipe-list/recipe-item/recipe-item.component';
+import {AppRecipeList} from '../app/recipes/recipe-list/recipe-list.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeader,
+    AppRecipeDetail,
+    AppRecipeItem,
+    AppRecipeList,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    AppRecipes
+
   ],
   imports: [
     BrowserModule
---
+++ src/app/recipes/recipe-list/recipeitem/recipe-item.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+@Component({
+  selector: 'app-recipeitem',
+  templateUrl: './recipeitem.component.html',
+  styleUrls: ['./recipeitem.component.css']
+})
+export class AppRecipeItem{
+
+}
