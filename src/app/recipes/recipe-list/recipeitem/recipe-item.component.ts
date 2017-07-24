86ae460ff89d7619b9ac7c8f2dfc24453922e6e6
Marwan-G <ghabin2004@gmail.com>
2017-07-24 22:24:27 +0200

    using nesting our app component

--- src/app/recipes/recipe-list/recipeitem/recipe-item.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-@Component({
-  selector: 'app-recipeitem',
-  templateUrl: './recipeitem.component.html',
-  styleUrls: ['./recipeitem.component.css']
-})
-export class AppRecipeItem{
-
-}
