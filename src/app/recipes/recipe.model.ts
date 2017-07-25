4ea9c9385dc3fc6a72900523add1dc156dbfca50
Marwan-G <ghabin2004@gmail.com>
2017-07-25 22:04:50 +0200

    Creating Recipe model

---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,11 @@
+export class Recipe{
+  public name:string
+  public description:string
+  public imagePath:string
+//construcot executed once you create new instance of Recipe class
+constructor(name:string, desc:string, imagepath:string){
+    this.name= name;
+    this.description= desc;
+    this.imagePath= imagepath;
+  }
+}
